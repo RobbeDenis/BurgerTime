442991b4508af2a532b47e708830cde377dd0019
RobbeDenis <77392406+RobbeDenis@users.noreply.github.com>
2022-05-29 01:34:52 +0200

    Added base animator

--- Minigin/Animator.cpp
+++ Minigin/Animator.cpp
@@ -1,2 +1,14 @@
 #include "RodEnginePCH.h"
 #include "Animator.h"
+#include "RenderComponent.h"
+
+dae::Animator::Animator(GameObject* gameObject)
+	: BaseComponent(gameObject)
+{
+
+}
+
+void dae::Animator::PostLoad()
+{
+	m_pRenderComponent = m_pGameObject->GetComponent<RenderComponent>();
+}
--- Minigin/Animator.h
+++ Minigin/Animator.h
@@ -3,14 +3,19 @@
 
 namespace dae
 {
+	class RenderComponent;
+
 	class Animator final : public BaseComponent
 	{
 	public:
+		void PostLoad() override;
 
 	private:
 		Animator(GameObject* gameObject);
 		template <typename T>
 		friend T* GameObject::AddComponent();
+
+		RenderComponent* m_pRenderComponent;
 	};
 }
 
